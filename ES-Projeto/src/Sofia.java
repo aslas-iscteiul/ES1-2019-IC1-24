1f30d8ef92747bacdf082e9c0663362a50c1c94d
Ana Sofia Alves <aslas@iscte-iul.pt>
2019-10-31 20:36:29 +0000

    Convert Java Project to Maven Project.

--- ES-Projeto/src/Sofia.java
+++ ES-Projeto/src/Sofia.java
@@ -1,4 +1,13 @@
 
 public class Sofia {
 
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	// https://www.youtube.com/watch?v=uv9tXFrTLtI
+	// https://www.youtube.com/watch?v=go3SsnvSpOo
+	// https://maven.apache.org/guides/getting-started/
+	// https://mvnrepository.com/artifact/org.apache.poi/poi/4.1.1
 }
