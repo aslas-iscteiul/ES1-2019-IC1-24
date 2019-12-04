b5c869ca498b746fd0212f637c60cf7aa04523fd
Ana Sofia Alves <aslas@iscte-iul.pt>
2019-12-04 18:08:35 +0000

    JavaDoc for LogicOperator class

--- ES-Projeto/src/app_structure/LogicOperator.java
+++ ES-Projeto/src/app_structure/LogicOperator.java
@@ -5,12 +5,11 @@ package app_structure;
 
 import java.util.NoSuchElementException;
 
-/**
- * @author ana0s
- *
- */
 public enum LogicOperator {
 	
+	/**
+	 * And logic operator (&&).
+	 */
 	AND("AND") {
 		@Override 
 		public boolean apply(boolean left, boolean right) {
@@ -18,6 +17,9 @@ public enum LogicOperator {
 		}
 	},
 	
+	/**
+	 * Or logic operator (||).
+	 */
 	OR("OR") {
 		@Override 
 		public boolean apply(boolean left, boolean right) {
@@ -27,6 +29,10 @@ public enum LogicOperator {
 
     private final String operator;
 
+    /**
+     * Sole constructor. Not used by programmers.
+     * @param operator - the name of enum constant.
+     */
     private LogicOperator(String operator) {
         this.operator = operator;
     }
@@ -36,6 +42,13 @@ public enum LogicOperator {
     	return "Operator: " + this.operator;
     }
     
+    /**
+     * Returns the enum constant of this type with the specified name.
+     * The string must match exactly an identifier used to declare an enum constant in this type.
+     * @param operator - string name of the enum constant.
+     * @return the enum constant with the specified name.
+     * @throws NoSuchElementException if this enum type has no constant with the specified name.
+     */
     public static LogicOperator parseOperator(String operator) {
         for (LogicOperator o : values()) {
             if (o.operator.equals(operator)) 
@@ -44,7 +57,13 @@ public enum LogicOperator {
         throw new NoSuchElementException(String.format("Unknown logic operator:", operator));
     }
     
-    
+    /**
+     * Compares two specified boolean conditions. 
+     * The result is true if and only if the left condition and/or (according to the logic operator) the right condition are true.
+     * Otherwise the result is false.
+     * @param left - The boolean left condition.
+     * @param right - The boolean right condition.
+     * @return true if the left condition and/or (according to the logic operator) the right condition are true. Otherwise returns false.
+     */
     public abstract boolean apply(boolean left, boolean right);
-
 }
