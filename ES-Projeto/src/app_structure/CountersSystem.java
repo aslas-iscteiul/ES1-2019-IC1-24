d656b8bb0efea51948f807cfe818b71f6cccbe1a
AntonioCarvalho <adcos1@iscte-iul.pt>
2019-12-05 01:51:57 +0000

    Create the javadoc of CountersSystem class

--- ES-Projeto/src/app_structure/CountersSystem.java
+++ ES-Projeto/src/app_structure/CountersSystem.java
@@ -1,8 +1,5 @@
-/**
- * 
- */
-package app_structure;
 
+package app_structure;
 /**
  * @author ana0s
  *
@@ -14,6 +11,9 @@ public class CountersSystem {
 	int adci;
 	int adii;
 	
+	/**
+	 * Create a CountersSistem where counters start with a value for 0.
+	 */
 	public CountersSystem() {
 		this.dci = 0;
 		this.dii = 0;
@@ -21,22 +21,42 @@ public class CountersSystem {
 		this.adii = 0;
 	}
 	
+	/**
+	 * Is a getter.
+	 * @return dci - value of "Defeitos Corrretamente Identificados"(DCI).
+	 */
 	public int getDCI() {
 		return dci;
 	}
 	
+	/**
+	 * Is a getter.
+	 * @return dii - value of "Defeitos Incorretamente identificados"(DII).
+	 */
 	public int getDII() {
 		return dii;
 	}
-		
+	
+	/**Is a getter.
+	 * @return adci - value of "Ausência de Defeitos Corretamente Identificados"(ADCI).
+	 */
 	public int getADCI() {
 		return this.adci;
 	}
 	
+	/**
+	 * Is a getter.
+	 * @return adii - value of "Aunsência de Defeitos Incorretamente Identificados"(ADII).
+	 */
 	public int getADII() {
 		return adii;
 	}
 	
+	/**
+	 * Through the various characteristics of each indicator, we account for the existence of each indicator and count the number of times that they occur.
+	 * @param long_method - boolean value taken the excel archive of the is_long_method column (8th) or the is_feature_envy column (11st).  
+	 * @param value - boolean value taken the excel archive of the ClientRule, PMD, IPlasma. 
+	 */
 	public void increment(boolean long_method, boolean value) {
 		if(long_method == true && value == true)
 			this.dci++;
@@ -51,6 +71,9 @@ public class CountersSystem {
 			this.adii++;
 	}
 	
+	/**
+	 * set the counters to 0.
+	 */
 	public void restart() {
 		this.dci = 0;
 		this.dii = 0;
@@ -58,6 +81,9 @@ public class CountersSystem {
 		this.adii = 0;
 	}
 
+	/**
+	 * Print counters values - (DCI), (DII), (ADCI) e (ADII).
+	 */
 	@Override
 	public String toString() {
 		return "DCI = " + this.dci + "\nDII = " + this.dii + "\nADCI = " + this.adci + "\nADII = " + this.adii;
